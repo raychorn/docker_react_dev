a05dae968108fc4dcc3e93f0704019408f415746
Ray C Horn <raychorn@gmail.com>
2022-10-14 12:17:03 +0000

    latest.

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+# find image with react/cli:latest
+IMAGEID=$(docker images | grep react/cli | grep latest | awk '{print $3}')
+if [[ -z $IMAGEID ]]
+then
+    echo "Cannot find react/cli:latest"
+else
+    echo "Found react/cli:latest with IMAGEID=$IMAGEID and remove it for the build."
+    docker rmi $IMAGEID
+fi
+
 docker build -t react/cli .
+
+docker image tag react/cli:latest 10.0.0.164:5000/react-cli:latest
+docker push 10.0.0.164:5000/react-cli:latest
+
+IMAGEID=$(docker images | grep react-cli | grep latest | awk '{print $3}')
+if [[ -z $IMAGEID ]]
+then
+    echo "Cannot find react/cli:latest"
+else
+    echo "Found react-cli:latest with IMAGEID=$IMAGEID so Build was successful."
+fi
